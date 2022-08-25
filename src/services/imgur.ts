902c38a97cb6bf1a4218bc52bca8c63d11ff1b49
ionaarregui <iona.arregui@gmail.com>
2022-08-24 23:16:20 -0300

    Prueba ingur 3

--- src/services/imgur.ts
+++ src/services/imgur.ts
@@ -49,14 +49,22 @@ export const sendImage = (file) => {
   var formdata = new FormData()
   formdata.append('image', 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
 
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: formdata
-  }
+  // var requestOptions = {
+  //   method: 'POST',
+  //   headers: myHeaders,
+  //   body: formdata
+  // }
 
-  fetch('https://api.imgur.com/3/image', requestOptions)
-    .then((response) => response.json())
-    .then((result) => console.log(result))
-    .catch((error) => console.log('error', error))
+  // fetch('https://api.imgur.com/3/image', requestOptions)
+  //   .then((response) => response.json())
+  //   .then((result) => console.log(result))
+  //   .catch((error) => console.log('error', error))
+
+  fetch('https://api.imgur.com/3/image', {
+    method: 'POST',
+    headers: {
+      Authorization: 'Client-ID 0afb7e3dcf88a75'
+    },
+    body: formdata
+  })
 }
