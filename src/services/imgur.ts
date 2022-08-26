4e90cc46eefe9ff0195b6b2cb0b81af95080027e
ionaarregui <iona.arregui@gmail.com>
2022-08-25 21:20:14 -0300

    fix imgur

--- src/services/imgur.ts
+++ src/services/imgur.ts
@@ -1,24 +1,22 @@
-import { image } from './imagePrueba'
+// export function getToken(file) {
+//   var formdata = new FormData()
+//   formdata.append('refresh_token', '89f2ce0e027e09261d5d65be0fc9d9b9807b3f3c')
+//   formdata.append('client_id', '46364885880e796')
+//   formdata.append('client_secret', '0a8fcffafef984c4693f1c922157b49bff249511')
+//   formdata.append('grant_type', 'refresh_token')
 
-export function getToken(file) {
-  var formdata = new FormData()
-  formdata.append('refresh_token', '89f2ce0e027e09261d5d65be0fc9d9b9807b3f3c')
-  formdata.append('client_id', '46364885880e796')
-  formdata.append('client_secret', '0a8fcffafef984c4693f1c922157b49bff249511')
-  formdata.append('grant_type', 'refresh_token')
+//   var requestOptions = {
+//     method: 'POST',
+//     body: formdata
 
-  var requestOptions = {
-    method: 'POST',
-    body: formdata
+//     //   body: JSON.stringify(formdata),
+//   }
 
-    //   body: JSON.stringify(formdata),
-  }
-
-  fetch('https://api.imgur.com/oauth2/token', requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log('error', error))
-}
+//   fetch('https://api.imgur.com/oauth2/token', requestOptions)
+//     .then((response) => response.text())
+//     .then((result) => console.log(result))
+//     .catch((error) => console.log('error', error))
+// }
 
 export const sendImage = (file) => {
   //   // axios.post("https://api.imgur.com/3/upload", config);
@@ -40,27 +38,17 @@ export const sendImage = (file) => {
   //     .then((response) => response.json())
   //     .then((result) => console.log(result))
   //     .catch((error) => console.log('error', error))
-  var myHeaders = new Headers()
-  myHeaders.append('Authorization', 'Client-ID 0afb7e3dcf88a75')
-  myHeaders.append('Content-Type', 'multipart/form-data; boundary=Wp4TJWrhcOFdZEw7')
-  myHeaders.append('Content-Length', '')
-  myHeaders.append('Host', '')
+  // var myHeaders = new Headers()
+  // myHeaders.append('Authorization', 'Client-ID 0afb7e3dcf88a75')
+  // myHeaders.append('Content-Type', 'multipart/form-data; boundary=Wp4TJWrhcOFdZEw7')
+  // myHeaders.append('Content-Length', '')
+  // myHeaders.append('Host', '')
 
-  var formdata = new FormData()
-  formdata.append('image', 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
+  const formdata = new FormData()
+  formdata.append('image', file)
+  // formdata.append('image', 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
 
-  // var requestOptions = {
-  //   method: 'POST',
-  //   headers: myHeaders,
-  //   body: formdata
-  // }
-
-  // fetch('https://api.imgur.com/3/image', requestOptions)
-  //   .then((response) => response.json())
-  //   .then((result) => console.log(result))
-  //   .catch((error) => console.log('error', error))
-
-  fetch('https://api.imgur.com/3/image', {
+  const result = fetch('https://api.imgur.com/3/image', {
     method: 'POST',
     headers: {
       Authorization: 'Client-ID 0afb7e3dcf88a75'
@@ -70,4 +58,7 @@ export const sendImage = (file) => {
     .then((response) => response.json())
     .then((result) => console.log(result))
     .catch((error) => console.log('error', error))
+
+  console.log(result)
+  return result
 }
